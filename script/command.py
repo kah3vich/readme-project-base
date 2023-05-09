8329ce627a6023b40846fab65a2623890a057b75
kah3vich <nik.b20@mail.ru>
2023-05-09 17:06:07 +0300

    feat: flow command for script

--- script/command.py
+++ script/command.py
@@ -1,15 +1,27 @@
 import json
+import sys
 
 from schema import schema_command
 
 
 def main():
+
+    rep_name = sys.argv[1:][0]
+
+    print('✅ rep_name    ', rep_name)
+
     f = open('bd.json')
     
     data = json.load(f)
     
     for i in data['bd']:
         path = './cache/command.txt'
+        title = i['title']
+
+        if bool(rep_name) and title == rep_name:
+            with open(path, 'w') as file:
+                file.write(schema_command(i))
+            return
 
         with open(path, 'w') as file:
             file.write(schema_command(i))
--- script/readme.md
+++ script/readme.md
@@ -3,3 +3,7 @@ python main.py - create readme file for projects
 python command.py - create bash script
 
 cat ./cache/command.txt | bash - active bash command
+
+```bash
+cd script/; python main.py; python command.py; cat ./cache/command.txt | bash; rm -rf cache/reps/*; cd ..;
+```
